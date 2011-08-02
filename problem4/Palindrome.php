e0a34636fb1ddab8d72712c91f114c7dd5a56911
Ketema Harris <ketema@ketema.net>
2011-08-01 23:52:53 -0400

    Moved solution to problem4 into its own file

--- problem4/Palindrome.php
+++ problem4/Palindrome.php
@@ -53,8 +53,5 @@ class Palindrome
 
 }
 
-$obj = new Palindrome();
-$maxPalindrome = $obj->getMaxPalindrome();
-echo "Max palindrome from product of 3 digit numbers = $maxPalindrome\n";
 
 ?>
---
+++ problem4/solution.php
@@ -0,0 +1,5 @@
+<?php
+
+$obj = new Palindrome();
+$maxPalindrome = $obj->getMaxPalindrome();
+echo "Max palindrome from product of 3 digit numbers = $maxPalindrome\n";
